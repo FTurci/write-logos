25c09281caa36b31fd7ff2519052bbdcaf83d083
Francesco Turci <turci@me.com>
2020-12-12 20:08:24 +0000

    First

--- src/writer.py
+++ src/writer.py
@@ -4,14 +4,15 @@ import svgwrite as svg
 
 
 width = 300
-height= 200
+height = 200
 
-dwg = svg.Drawing(filename = "test-svgwrite.svg",
-                                size = (f"{width}px", f"{height}px"))
+dwg = svg.Drawing(filename="test-svgwrite.svg", size=(f"{width}px", f"{height}px"))
 
-dwg.add_stylesheet("style.css", title="") # same rules as for html files
-
+dwg.add_stylesheet("style.css", title="")  # same rules as for html files
+dwg.embed_font("Zapfino", "/System/Library/Fonts/Supplemental/Zapfino.ttf")
 g = dwg.g(class_="mystyle")
-g.add(dwg.text("eXtemporanea", insert=(width*0.4, height*0.4))) # settings are valid for all text added to 'g'
+g.add(
+    dwg.text("eXtemporanea", insert=(width * 0.4, height * 0.4))
+)  # settings are valid for all text added to 'g'
 dwg.add(g)
 dwg.save()
