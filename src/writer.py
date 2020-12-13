2936bdaca09a09daa09be6a1451c7267e046a1e4
Francesco Turci <turci@me.com>
2020-12-13 11:20:52 +0000

    with comments

--- src/writer.py
+++ src/writer.py
@@ -8,9 +8,7 @@ height = 200
 
 dwg = svg.Drawing(filename="in.svg", size=(f"{width}px", f"{height}px"))
 
-# g = dwg.g(style="font-size:30;font-family:Zapfino;font-weight:bold;font-style:oblique;stroke:black;stroke-width:1;fill:none")
-
-# dwg.defs.add(dwg.style(' @import url("https://fonts.googleapis.com/css?family=Pacifico:400,400i,700,700i");'))
+# Define style: here we include the pah to the custom font
 dwg.defs.add(
     dwg.style(
         """    
@@ -18,17 +16,14 @@ dwg.defs.add(
  src: url('fonts/SCRIPTIN.ttf');}"""
     )
 )
-g = dwg.g(style="font-size:30;font-family:Scriptina;")
+# use the style
+g = dwg.g(style="font-size:30;font-family:Scriptina;font-weight:bold;font-style:oblique;stroke:black;stroke-width:1;fill:red")
 g.add(dwg.text("eXtemporanea", insert=(width * 0.4, height * 0.4)))
 # settings are valid for all text added to 'g'
 dwg.add(g)
 dwg.save()
-
-print(dwg.tostring())
-# trick to convert the svg with fonts to a path
-
+# trick to convert the svg with fonts to a path: usingh inkscape
 import subprocess
-
 subprocess.call(
     "inkscape in.svg --export-text-to-path --export-plain-svg -o out.svg", shell=True
 )
