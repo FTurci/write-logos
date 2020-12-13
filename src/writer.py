3c730191143e23fed76062d2ef7c0f2df362af2d
Francesco Turci <turci@me.com>
2020-12-13 10:45:35 +0000

    First

--- src/writer.py
+++ src/writer.py
@@ -1,18 +1,22 @@
 import svgwrite as svg
+# check this to change fonts https://stackoverflow.com/questions/17127083/python-svgwrite-and-font-styles-sizes
 
-# check this to change fonts
 
-
-width = 300
+width = 500
 height = 200
 
-dwg = svg.Drawing(filename="test-svgwrite.svg", size=(f"{width}px", f"{height}px"))
+dwg = svg.Drawing(filename="in.svg", size=(f"{width}px", f"{height}px"))
 
-dwg.add_stylesheet("style.css", title="")  # same rules as for html files
-dwg.embed_font("Zapfino", "/System/Library/Fonts/Supplemental/Zapfino.ttf")
-g = dwg.g(class_="mystyle")
+g = dwg.g(style="font-size:30;font-family:Zapfino;font-weight:bold;font-style:oblique;stroke:black;stroke-width:1;fill:none")
+
 g.add(
     dwg.text("eXtemporanea", insert=(width * 0.4, height * 0.4))
 )  # settings are valid for all text added to 'g'
 dwg.add(g)
 dwg.save()
+#trick o convert the svg with fonts to a path
+import subprocess
+subprocess.call("inkscape in.svg --export-text-to-path --export-plain-svg -o out.svg", shell = True)
+
+
+
